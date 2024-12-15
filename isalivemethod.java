c5bbfdab443cfe430b4a6aaca0ce81d2e2e4367a
Kolla Greeshma Sree <kollagreeshmasree2@gmail.com>
2024-12-15 11:23:10 +0530

    Add files via upload

---
+++ isalivemethod.java
@@ -0,0 +1,10 @@
+public class isalivemethod
+{
+    public static void main(String args[])
+    {
+        Runnable r=()->System.out.println("I am new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+        System.out.println(t.isAlive());
+    }
+}
