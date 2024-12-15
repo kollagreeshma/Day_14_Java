3835b2b10467531c78974b603021bd82a6472e62
Kolla Greeshma Sree <kollagreeshmasree2@gmail.com>
2024-12-15 11:20:39 +0530

    Add files via upload

---
+++ creatingthreadusinglambda.java
@@ -0,0 +1,9 @@
+public class creatingthreadusinglambda
+{
+    public static void main(String args[])
+    {
+        Runnable r=()->System.out.println("I am new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+    }
+}
